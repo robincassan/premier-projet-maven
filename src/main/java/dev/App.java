f9f6a1f86a40c6801efd55198b77cf9ed240a4a6
Robin CASSAN <robin.cassan@netc.eu>
2019-05-31 15:40:54 +0200

    4eme commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,20 +1,16 @@
 package dev;
 
-import java.io.IOException;
+import java.util.ResourceBundle;
 
 import com.github.lalyos.jfiglet.FigletFont;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws IOException
-    {
-    	 String asciiArt = FigletFont.convertOneLine("a hello");
-    	    System.out.println(asciiArt);
-    	  }
-    	
-    
+public class App {
+	public static void main(String[] args) {
+		String titre = ResourceBundle.getBundle("application").getString("titre");
+		String asciiArt = FigletFont.convertOneLine(titre);
+		System.out.println(asciiArt);
+// affichage de l'environnement
+		String environnement = ResourceBundle.getBundle("application").getString("environnement");
+		System.out.println("Environnement : " + environnement);
+	}
 }
