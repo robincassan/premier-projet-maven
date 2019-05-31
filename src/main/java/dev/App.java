d13d43a39f4ad3b1116c0ce0d3d98e4aec497444
Robin CASSAN <robin.cassan@netc.eu>
2019-05-31 14:44:19 +0200

    troisieme commit

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,13 +1,20 @@
 package dev;
 
+import java.io.IOException;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
-    }
+    	 String asciiArt = FigletFont.convertOneLine("a hello");
+    	    System.out.println(asciiArt);
+    	  }
+    	
+    
 }
